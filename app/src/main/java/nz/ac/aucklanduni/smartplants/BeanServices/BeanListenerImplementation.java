c087edbd97de46f5dbf481a357f5586ccc1b1d31
Samanyu Kansara <skan729@aucklanduni.ac.nz>
2015-10-16 17:40:29 +1300

    Code to separate different types of data received.

--- app/src/main/java/nz/ac/aucklanduni/smartplants/BeanServices/BeanListenerImplementation.java
+++ app/src/main/java/nz/ac/aucklanduni/smartplants/BeanServices/BeanListenerImplementation.java
@@ -10,6 +10,9 @@ import com.punchthrough.bean.sdk.message.ScratchBank;
  * Created by mark on 10/16/2015.
  */
 public class BeanListenerImplementation implements BeanListener {
+
+    int count = 1;
+
     @Override
     public void onConnected() {
 
@@ -29,6 +32,17 @@ public class BeanListenerImplementation implements BeanListener {
     public void onSerialMessageReceived(byte[] bytes) {
         String string = new String(bytes);
         Log.i("serial message", string);
+
+        if (!string.trim().equals("")){
+            if (count % 3 == 1) {
+                //Save temperature
+            }else if(count % 3 == 2) {
+                //Save light
+            }else{
+                //Save water
+            }
+            count++;
+        }
     }
 
     @Override
